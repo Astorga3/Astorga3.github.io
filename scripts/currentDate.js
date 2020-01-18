bfc8b498c441a925f0afa16dbfed96ee72b36aa5
AStorga3 <ast17003@byui.edu>
2020-01-17 18:05:34 -0700

    hello

---
+++ scripts/currentDate.js
@@ -0,0 +1,6 @@
+const currentDate = new Date();
+const year = currentDate.getFullYear();
+
+document.getElementById("currentYear").innerHTML = year;
+
+document.getElementById("lastModified").innerHTML = document.lastModified;
