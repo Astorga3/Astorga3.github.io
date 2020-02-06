863fddc56475cc848368a19c19a147989dd6b0af
AStorga3 <ast17003@byui.edu>
2020-02-06 10:54:19 -0700

    hello

--- lesson5/scripts/main.js
+++ lesson5/scripts/main.js
@@ -30,3 +30,14 @@ function hamMenu(){
 document.querySelector('#current-temp').innerHTML = '50&deg;';
 document.querySelector('#wind-speed').innerHTML = '10mph;';
 document.querySelector('#humidity').innerHTML = '10&percnt;';
+
+/*pancake*/
+
+const currentDate = new Date();
+const aside =  document.querySelector('aside');
+
+if(currentDate.getDay() === 5){
+    aside.style.display = 'block';
+} else {
+  aside.style.display = 'none';
+}
