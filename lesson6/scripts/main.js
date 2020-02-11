11f21669e1a470f385325b3a193ef78351b77b87
AStorga3 <ast17003@byui.edu>
2020-02-11 11:09:08 -0700

    hello

---
+++ lesson6/scripts/main.js
@@ -0,0 +1,42 @@
+ /*declare date variable*/ 
+let currentDate = new Date();
+let fullDate;
+
+/*day of week*/
+let daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+let dayOfWeek = currentDate.getDay();
+fullDate = daysOfWeek[dayOfWeek];
+
+
+/*day of month*/
+let dayOfMonth = currentDate.getDate();
+fullDate += ', ' + dayOfMonth;
+
+/*month*/
+let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+fullDate += ' ' + months[currentDate.getMonth()];  
+
+/*year*/
+let year = currentDate.getFullYear();
+fullDate += ' ' + year;
+
+/*full date*/
+
+document.querySelector('#current-date').textContent = fullDate;
+
+function hamMenu(){
+  document.querySelector(".flexnavigation").classList.toggle("responsive");  
+}
+document.querySelector('#current-temp').innerHTML = '50&deg;';
+document.querySelector('#wind-speed').innerHTML = '10mph;';
+document.querySelector('#humidity').innerHTML = '10&percnt;';
+
+/*pancake*/
+
+const aside =  document.querySelector('aside');
+
+if(currentDate.getDay() === 4){
+    aside.style.display = 'block';
+} else {
+  aside.style.display = 'none';
+}
